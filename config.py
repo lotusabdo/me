8aa2063dc39322c20459a8e09d4fb55704d0d558
me2wdo <159659732+me2wdo@users.noreply.github.com>
2024-02-13 22:05:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/me2wdo/m3edo.git",
+    "https://github.com/me2wdo/medo.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
