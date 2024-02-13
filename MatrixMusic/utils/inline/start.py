74b21d8e950fbd001cc940515fc83748b7ea777d
me2wdo <159659732+me2wdo@users.noreply.github.com>
2024-02-13 21:56:52 +0200

    Add files via upload

---
+++ MatrixMusic/utils/inline/start.py
@@ -0,0 +1,37 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from MatrixMusic import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅", url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text="𝙶𝚁𝙾̀𝚄𝙿", url= "https://t.me/Q1_QU"),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text="اوامــــر الـــبـــوت📚", callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text="مطور البوت", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="𝙶𝚁𝙾̀𝚄𝙿", url=f"https://t.me/Q1_QU"), 
+        ],
+        [
+            InlineKeyboardButton(text="ألموع ـلم مـ ــيــ ــدؤ ســ ـــنــ ــدأل >3`", url=f"https://t.me/I1_35"), 
+            InlineKeyboardButton(text="𝚂́𝙾𝚄𝚁𝙲𝙴 𝚁𝙸𝙽𝙾", url=f"https://t.me/I1_35") , 
+        ],
+    ]
+    return buttons
