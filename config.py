3800ac631ff8f373bc2e8bdd2201cad6d0b3e422
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-08-30 20:40:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lotusabdo/medo.git",
+    "https://github.com/lotusabdo/med.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
